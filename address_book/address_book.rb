d6cd23289718b572da7de13f367d8b6d0687ddef
Jose Rosado <jrosadocruz@gmail.com>
2015-03-10 09:35:18 -0400

    Added add contacts and search

--- address_book/address_book.rb
+++ address_book/address_book.rb
@@ -8,8 +8,73 @@ class AddressBook
     @contacts = []
   end
 
+  def run
+    loop do 
+      puts "Address Book"
+      puts "A: add new contact"
+      puts "S: search for contact"
+      puts "E: to exit"
+      print "Enter your choice: "
+      input = gets.chomp.downcase
+      case input
+      when 'a'
+        add_contact
+      when 's'
+        print "Search term: "
+        search = gets.chomp
+        find_by_name(search)
+        find_by_phone(search)
+        find_by_address(search)
+      else 'e'
+        break
+      end
+    end
+  end
+
   def add_contact
-    
+    contact = Contact.new
+    print "First name: "
+    contact.first_name = gets.chomp
+    print "Middle name: "
+    contact.middle_name = gets.chomp
+    print "Last name: "
+    contact.last_name = gets.chomp
+
+    loop do
+      puts "Add phone number or address?"
+      puts "p: Add phone number"
+      puts "a: Add address"
+      puts "(Any other key to go back)"
+      responce = gets.chomp.downcase
+      case responce
+      when 'p'
+        phone = PhoneNumber.new
+        print "Phone number kind (Home, Work, Etc): "
+        phone.kind = gets.chomp
+        print "Number: "
+        phone.number = gets.chomp
+        contact.phone_numbers << phone
+      when 'a'
+        address = Address.new
+        print "Address kind (Home, Work, Etc): "
+        address.kind = gets.chomp
+        print "Address line 1: "
+        address.street_1 = gets.chomp
+        print "Address line 2: "
+        address.street_2 = gets.chomp
+        print "City: "
+        address.city = gets.chomp
+        print "State: "
+        address.state = gets.chomp
+        print "Postal code: "
+        address.postal_code = gets.chomp
+        contact.addresses << address
+      else 
+        print "\n"
+        break
+      end
+    end
+    @contacts << contact
   end
 
   def print_contacts
@@ -36,13 +101,12 @@ class AddressBook
       end
     end
 
-    print_results("Search result for (#{name})", results)
+    print_results("Name search result for (#{name})", results)
   end
 
   def find_by_phone(phone_number)
     results = []
     search  = phone_number.gsub(/[-.]/, '')
-    i = 0
     @contacts.each do |contact|
       contact.phone_numbers.each do |phone_number|
         if phone_number.number.gsub(/[-.]/, '').include?(search)
@@ -51,7 +115,22 @@ class AddressBook
       end
     end
 
-    print_results("Search result for (#{phone_number})", results)
+    print_results("Phone search result for (#{phone_number})", results)
+  end
+
+  def find_by_address(address)
+    results = []
+    search  = address.downcase
+
+    @contacts.each do |contact|
+      contact.addresses.each do |address|
+        if address.to_s('long').downcase.include?(search)
+          results << contact unless results.include?(contact)
+        end
+      end
+    end
+
+    print_results("Address search result for (#{address})", results)
   end
 
   def print_results(msg, array)
@@ -67,7 +146,6 @@ class AddressBook
 end
 
 address_book = AddressBook.new
-
 jose = Contact.new
 jose.first_name = "José"
 jose.middle_name = "Ricardo"
@@ -85,6 +163,7 @@ brenda.add_address('Home', 'Calle 21 no.17', '', 'Santo Domingo', 'D.N.', '00000
 
 address_book.contacts.push(jose)
 address_book.contacts.push(brenda)
+address_book.run
 # address_book.contacts.push(brenda)
 
 # puts jose.to_s('full_name')
@@ -92,5 +171,6 @@ address_book.contacts.push(brenda)
 # jose.print_addresses
 
 # address_book.print_contacts
-address_book.find_by_name('a')
+# address_book.find_by_name('a')
 # address_book.find_by_phone('8')
+# address_book.find_by_address('Santo')
